771d76e7c9635eec8514f83ba96aadfb026323cd
Azeez Ojeyinka <azeezojeyinka@yahoo.com>
2021-11-23 17:45:42 +0100

    Task3: Creating a service to Get album data

---
+++ src/app/product.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs';
+// import { Album } from './album';
+import 'rxjs/add/operator/map'
+
+
+@Injectable(
+  // {providedIn: 'root'}
+  )
+export class ProductService {
+  private _albumUrl = '../assets/album.json'
+
+  constructor(private _http: Http) { 
+     
+  }
+
+  getAlbum(id:number) {
+    return this._http.get(this._albumUrl).map(response => response.json())
+  }
+
+
+}
+
