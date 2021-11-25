82a1fc68eafbe11687aa9c530eedad2c63a39fba
Azeez Ojeyinka <azeezojeyinka@yahoo.com>
2021-11-26 00:03:02 +0100

    Task 4, Using interface to describe Data

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs';
-// import { Album } from './album';
+import { Observable } from 'rxjs/Observable';
+import { Album } from './album';
 import 'rxjs/add/operator/map'
 
 
@@ -15,8 +15,8 @@ export class ProductService {
      
   }
 
-  getAlbum(id:number) {
-    return this._http.get(this._albumUrl).map(response => response.json())
+  getAlbum(id:number):Observable<Album> {
+    return this._http.get(this._albumUrl).map(response => <Album>response.json())
   }
 
 
